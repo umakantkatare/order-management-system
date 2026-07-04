d6169114fbec9938831a7d17864baa8178c2b28b
umakant katare <umakantkatare11@gmail.com>
2026-07-05 01:34:06 +0530

    configure axios client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axiosInstance from "./api/axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const testConnection = async () => {
+    try {
+      const response = await axiosInstance.get("/orders");
+
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div>hello world</div>
-  )
+    <div className="p-10">
+      <button
+        onClick={testConnection}
+        className="rounded bg-blue-600 px-4 py-2 text-white"
+      >
+        Test Backend Connection
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
