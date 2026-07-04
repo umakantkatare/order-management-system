2aeeebd12324f896d7c6e97101c041032586b28c
umakant katare <umakantkatare11@gmail.com>
2026-07-05 01:43:34 +0530

    implement orders hook

---
+++ frontend/src/hooks/useOrders.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { getOrders } from "../api/order.api";
+
+const useOrders = () => {
+  const [orders, setOrders] = useState([]);
+  const [status, setStatus] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchOrders = async (selectedStatus = status) => {
+    try {
+      setLoading(true);
+      setError("");
+
+      const response = await getOrders(selectedStatus);
+
+      setOrders(response.data);
+    } catch (err) {
+      setError(err.response?.data?.message || "Failed to fetch orders.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return {
+    orders,
+    loading,
+    error,
+    status,
+    setStatus,
+    fetchOrders,
+  };
+};
+
+export default useOrders;
